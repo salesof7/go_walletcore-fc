c79fab38de299c6e73b45a30b0c1434d9f60ca67
salesof7 <jpsaless2002@gmail.com>
2023-06-23 00:38:40 -0300

    feat: create account entity

---
+++ internal/entity/account.go
@@ -0,0 +1,29 @@
+package entity
+
+import (
+	"time"
+
+	uuid "github.com/google/uuid"
+)
+
+type Account struct {
+	ID        string
+	Client    *Client
+	Balance   float64
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func NewAccount(client *Client) *Account {
+	if client == nil {
+		return nil
+	}
+	account := &Account{
+		ID:        uuid.New().String(),
+		Client:    client,
+		Balance:   0,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	return account
+}
